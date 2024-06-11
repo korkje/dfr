95095db4ff7f79eb139fc5e808de5a57c611a14a
Ola Næss Kaldestad <mail@okal.no>
2024-06-11 20:44:27 +0200

    cleanup

--- tests/dfr.test.ts
+++ tests/dfr.test.ts
@@ -1,12 +1,26 @@
-import { assert } from "@std/assert";
-import dfr from "../lib/dfr.ts";
+import { assertEquals, assertInstanceOf, assertRejects,  } from "@std/assert";
+import dfr from "lib/dfr.ts";
 
-Deno.test("dfr", async () => {
+Deno.test("dfr", () => {
     const { promise, resolve, reject } = dfr<number>();
 
-    assert(promise instanceof Promise);
-    assert(typeof resolve === "function");
-    assert(typeof reject === "function");
+    assertInstanceOf(promise, Promise);
+    assertEquals(typeof resolve, "function");
+    assertEquals(typeof reject, "function");
+});
+
+Deno.test("resolve", async () => {
+    const { promise, resolve } = dfr<number>();
+
     resolve(42);
-    assert(await promise === 42);
+
+    assertEquals(await promise, 42);
+});
+
+Deno.test("reject", () => {
+    const { promise, reject } = dfr<number, string>();
+
+    reject("error");
+
+    assertRejects(() => promise, "error");
 });
